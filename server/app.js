ed3e9f97c605909e2b62f0fa2dd5f54e1eb0ea8d
David Muriel <dmuriel@bitergia.com>
2014-11-24 12:35:28 +0100

    Update app port log message.
    When using passenger, the server.address() call is undefined and fails,
    so we just use the port value we've set before.

--- server/app.js
+++ server/app.js
@@ -40,5 +40,5 @@ app.get('/api/ckan/organizations',ckan.organizations);
 // start server
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + app.get('port'));
 });
