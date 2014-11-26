342f7da35173533592933768972022afa4944922
Alvaro del Castillo <acs@bitergia.com>
2014-11-26 11:11:51 +0100

    Based web desing in http://startbootstrap.com/template-overviews/sb-admin/.

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('chanchanApp', [
   'ngRoute',
+  'ui.bootstrap',
   'chanchanApp.ckan',
   'chanchanApp.orion',
   'chanchanApp.version'
@@ -10,3 +11,26 @@ angular.module('chanchanApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/ckan'});
 }]);
+
+angular.module('chanchanApp').controller('DropdownCtrl', function ($scope, $log) {
+$scope.items = [
+                {type:"fa-user",value:"Profile"},
+                {type:"fa-envelope",value:"Inbox"},
+                {type:"fa-gear",value:"Settings"},
+                {type:"fa-power-off",value:"Log Out"}
+];
+
+$scope.status = {
+  isopen: false
+};
+
+$scope.toggled = function(open) {
+  $log.log('Dropdown is now: ', open);
+};
+
+$scope.toggleDropdown = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.status.isopen = !$scope.status.isopen;
+    };
+});
