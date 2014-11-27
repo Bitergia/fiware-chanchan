08ab523bb6e274b8e6ed8900699f5c5723df6d64
David Muriel <dmuriel@bitergia.com>
2014-11-27 21:06:19 +0100

    Revert config.js.

--- server/config.js
+++ server/config.js
@@ -2,21 +2,20 @@ var config = {}
 
 // URL to the FI-WARE Identity Management GE
 // default: https://account.lab.fi-ware.org
-// config.idm_url = 'https://account.lab.fi-ware.org';
-config.idm_url = 'https://idm.shinchan.bitergia.org';
+config.idm_url = 'https://account.lab.fi-ware.org';
 
 // Oauth2 configuration
 // Found on the application profile page after registering
 // the application on the FI-WARE Identity Management GE
 
 // Client ID for the application
-config.client_id = '3';
+config.client_id = '';
 
 // Client Secret for the application
-config.client_secret = 'b91f39f836e1cd7d1deae1e0d184ea09976a066bd0a6e4361e8ffe88422bbd677706982fa2feaa85f3bd291e810c30e616a4df10e3e659f823d42e8a5146ba9f';
+config.client_secret = '';
 
 // Callback URL for the application
-config.callback_url = 'http://localhost:3000/login';
+config.callback_url = '';
 
 
 module.exports = config;
