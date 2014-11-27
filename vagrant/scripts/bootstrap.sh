6fdddafb092031894498f680d6352e43e16f9d8c
David Muriel <dmuriel@bitergia.com>
2014-11-27 20:49:10 +0100

    Bootstrap script for vagrant provisioning.

---
+++ vagrant/scripts/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export SCRIPTS_PATH="/vagrant/scripts"
+export UTILS_PATH="/vagrant/scripts/util"
+cd ${SCRIPTS_PATH}
+
+# load environment variables
+source variables.sh
+
+# load packages
+bash packages.sh
+
+# install IDM
+bash install-idm.sh
+
+# install Chanchan
+bash install-chanchan.sh
+
+# install Cygnus
+bash install-cygnus.sh
+
+# install Orion
+bash install-orion.sh
