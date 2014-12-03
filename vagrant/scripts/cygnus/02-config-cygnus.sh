77dc775a30e4b1ad5951530892cc8d70013d9203
Alvaro del Castillo <acs@bitergia.com>
2014-12-03 07:24:21 +0100

    New config for cygnus creating 2 new CKAN sinks for org1 and org2 CKAN orgs.

--- vagrant/scripts/cygnus/02-config-cygnus.sh
+++ vagrant/scripts/cygnus/02-config-cygnus.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # copy template config
-cp ${HOME}/fiware-connectors/flume/conf/cygnus.conf.template ${HOME}/${APACHE_FLUME_HOME}/conf/cygnus.conf
+# cp ${HOME}/fiware-connectors/flume/conf/cygnus.conf.template ${HOME}/${APACHE_FLUME_HOME}/conf/cygnus.conf
 
 # edit config
-sed -i ${HOME}/${APACHE_FLUME_HOME}/conf/cygnus.conf \
-    -e "s/cygnusagent.sinks = hdfs-sink mysql-sink ckan-sink/cygnusagent.sinks = ckan-sink/" \
-    -e "s/cygnusagent.channels = hdfs-channel mysql-channel ckan-channel/cygnusagent.channels = ckan-channel/" \
-    -e "s/org42/${CKAN_ORGANIZATION}/g" \
-    -e "s/ckan_host = x.y.z.w/ckan_host = demo.ckan.org/g" \
-    -e "s/api_key = ckanapikey/api_key = ${CKAN_API_KEY}/g" \
-    -e "s/default_dataset = mydataset/default_dataset = ${CKAN_DATASET}/g"
+# sed -i ${HOME}/${APACHE_FLUME_HOME}/conf/cygnus.conf \
+#     -e "s/cygnusagent.sinks = hdfs-sink mysql-sink ckan-sink/cygnusagent.sinks = ckan-sink/" \
+#     -e "s/cygnusagent.channels = hdfs-channel mysql-channel ckan-channel/cygnusagent.channels = ckan-channel/" \
+#     -e "s/org42/${CKAN_ORGANIZATION}/g" \
+#     -e "s/ckan_host = x.y.z.w/ckan_host = demo.ckan.org/g" \
+#     -e "s/api_key = ckanapikey/api_key = ${CKAN_API_KEY}/g" \
+#     -e "s/default_dataset = mydataset/default_dataset = ${CKAN_DATASET}/g"
+
+# Copy the preconfigured config file directly
+echo cp ${SCRIPTS_PATH}/cygnus/cygnus.conf ${HOME}/${APACHE_FLUME_HOME}/conf/cygnus.conf
--- vagrant/scripts/cygnus/04-notify-contex-cygnus.sh
+++ vagrant/scripts/cygnus/04-notify-contex-cygnus.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-# Subscribe Room context and Room1 entity to notify CKAN on changes
+# org1: Subscribe Room context and Room1 entity to notify CKAN on changes
 (curl localhost:10026/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
 {
     "entities": [
         {
-            "type": "Room",
+            "type": "org1Room",
             "isPattern": "false",
-            "id": "Room1" 
+            "id": "Room1"
         }
     ],
     "attributes": [
-        "temperature" 
+        "temperature"
     ],
-    "reference": "http://localhost:5050/notify",
+    "reference": "http://localhost:5001/notify",
     "duration": "P1M",
     "notifyConditions": [
         {
             "type": "ONCHANGE",
             "condValues": [
-                "pressure" 
+                "pressure"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
+
+# org2: Subscribe Room context and Room1 entity to notify CKAN on changes
+(curl localhost:10026/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "org2Room",
+            "isPattern": "false",
+            "id": "Room1"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "http://localhost:5002/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "pressure"
             ]
         }
     ],
@@ -27,28 +54,54 @@
 }
 EOF
 
-# Append sample update context to check CKAN is notified
+# org1: Append sample update context to check CKAN is notified
 (curl localhost:10026/NGSI10/updateContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
 {
     "contextElements": [
         {
-            "type": "Room",
+            "type": "org1Room",
             "isPattern": "false",
             "id": "Room1",
             "attributes": [
             {
                 "name": "temperature",
                 "type": "centigrade",
-                "value": "0.01" 
+                "value": "0.01"
             },
             {
                 "name": "pressure",
                 "type": "mmHg",
-                "value": "720" 
+                "value": "720"
             }
             ]
         }
     ],
-    "updateAction": "APPEND" 
+    "updateAction": "APPEND"
+}
+EOF
+
+# org2: Append sample update context to check CKAN is notified
+(curl localhost:10026/NGSI10/updateContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "contextElements": [
+        {
+            "type": "org2Room",
+            "isPattern": "false",
+            "id": "Room1",
+            "attributes": [
+            {
+                "name": "temperature",
+                "type": "centigrade",
+                "value": "0.01"
+            },
+            {
+                "name": "pressure",
+                "type": "mmHg",
+                "value": "720"
+            }
+            ]
+        }
+    ],
+    "updateAction": "APPEND"
 }
 EOF
