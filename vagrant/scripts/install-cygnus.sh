b93f09d2526ca30d012db45056c1f184fa7a799d
Alvaro del Castillo <acs@bitergia.com>
2014-12-03 07:23:31 +0100

    Just execute "*.sh" files, so we can store also config files in scripts dir.

--- vagrant/scripts/install-cygnus.sh
+++ vagrant/scripts/install-cygnus.sh
@@ -5,7 +5,7 @@ export CYGNUS_SCRIPTS="${SCRIPTS_PATH}/cygnus"
 su - ${CYGNUS_USER} <<EOF
 # this is needed to be able to use the variables with the ${CYGNUS_USER} user
 source ${SCRIPTS_PATH}/variables.sh
-for f in \$( ls ${CYGNUS_SCRIPTS}/* ) ; do
+for f in \$( ls ${CYGNUS_SCRIPTS}/*.sh ) ; do
     bash \${f}
 done
 EOF
