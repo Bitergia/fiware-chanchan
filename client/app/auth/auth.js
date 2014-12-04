bf417965eb4403153d41a353503fb4502b4c892d
Alvaro del Castillo <acs@bitergia.com>
2014-12-04 10:02:20 +0000

    Show nice message with the result of login process. Refactored organizations data.

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -15,28 +15,29 @@ angular.module('chanchanApp.auth', ['ngRoute'])
     // The app is registered in an organization
     $scope.user = "chanchan@idm.server";
     $scope.password = "ccadmin";
-    $scope.organizations = ["org1","org2"]; // Will be used the app for this org
-
+    // The id and secret comes from IDM application data in the org
+    $scope.organizations = {"org1": {
+                                "name":"org1",
+                                "id":"4",
+                                "secret":"86212b0096f190047cc321ef021ca7649b8ef0bc5da1c689f588512c62504d3152ff6ea2b80919de9ad3489c647cee4c8c250fc6eeef9a78c425a595064401d3",
+                            },
+                            "org2": {
+                                "name":"org2",
+                                "id":"3",
+                                "secret":"60236d3eb659b8ad3259658ed6b3a2c85dede8c87110e8e2e81dab267cfb5db59d900ec6e00b7aab482a9b7d4b95b1e88096c1b6777a3417c34a325eef005678",
+                            }};
    if ($scope.user_data !== undefined) {
 	$location.path("/ckan");
    }
 
     $scope.auth = function() {
-        var app_org2_secret = "60236d3eb659b8ad3259658ed6b3a2c85dede8c87110e8e2e81dab267cfb5db59d900ec6e00b7aab482a9b7d4b95b1e88096c1b6777a3417c34a325eef005678";
-        var app_org2_id = '3';
-        var app_org1_secret = "86212b0096f190047cc321ef021ca7649b8ef0bc5da1c689f588512c62504d3152ff6ea2b80919de9ad3489c647cee4c8c250fc6eeef9a78c425a595064401d3";
-        var app_org1_id = '4';
-        var data =  'grant_type=password';
-        data += '&username=chanchan@idm.server';
-        data += '&password=ccadmin';
+        var data = 'grant_type=password';
+        data +=    '&username='+$scope.user;
+        data +=    '&password='+$scope.password;
 
-	    if ($scope.organization === "org1") {
-            data += '&client_id='+app_org1_id;
-            data += '&client_secret='+app_org1_secret;
-	    } else if ($scope.organization === "org2") {
-            data += '&client_id='+app_org2_id;
-            data += '&client_secret='+app_org2_secret;
-        }
+        var org_data = $scope.organizations[$scope.organization];
+        data += '&client_id='+org_data.id;
+        data += '&client_secret='+org_data.secret;
 
         var url = "https://idm.server";
         var oauth_token = "oauth2/token";
@@ -48,10 +49,12 @@ angular.module('chanchanApp.auth', ['ngRoute'])
             $http({method:'GET',url:url+"/"+user_roles+"?access_token="+data.access_token})
             .success(function(data,status,headers,config){
                $scope.user_data =  data;
+               $scope.auth_result = "ok";
                console.log(data);
 	        });
         }).
         error(function(data,status,headers,config){
+          $scope.auth_result = "error";
           console.log(data)
         });
     };
