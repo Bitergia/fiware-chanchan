759ef7dcc9a94c819d3393daefb062edb47c5466
Alvaro del Castillo <acs@bitergia.com>
2014-12-04 07:31:03 +0000

    Removed cookies module. Not used. Get auth token in selected organization and show the user data for this token.

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -16,36 +16,43 @@ angular.module('chanchanApp.auth', ['ngRoute'])
     $scope.user = "chanchan@idm.server";
     $scope.password = "ccadmin";
     $scope.organizations = ["org1","org2"]; // Will be used the app for this org
+
+   if ($scope.user_data !== undefined) {
+	$location.path("/ckan");
+   }
+
     $scope.auth = function() {
-        // curl --insecure -i -X POST -H "Content-Type: application/x-www-form-urlencoded" https://idm.server/oauth2/token 
-        // -d 'grant_type=password&username=chanchan@idm.server&password=ccadmin&client_id=2&client_secret=6a928e79291c04042271fcf06edc20ffa15f2a2bd18ef332a7d5b2a2fcc3368149722447995cb68d0c94aca5acb82e37d1b269a36530db0d7b2a9cdfd51cbfa5'
+        var app_org2_secret = "60236d3eb659b8ad3259658ed6b3a2c85dede8c87110e8e2e81dab267cfb5db59d900ec6e00b7aab482a9b7d4b95b1e88096c1b6777a3417c34a325eef005678";
+        var app_org2_id = '3';
+        var app_org1_secret = "86212b0096f190047cc321ef021ca7649b8ef0bc5da1c689f588512c62504d3152ff6ea2b80919de9ad3489c647cee4c8c250fc6eeef9a78c425a595064401d3";
+        var app_org1_id = '4';
+        var data =  'grant_type=password';
+        data += '&username=chanchan@idm.server';
+        data += '&password=ccadmin';
 
-        var formData = {
-            'grant_type' : "password",
-            'username' : $scope.user,
-            'password' : $scope.password,
-            'client_id' : 2,
-            'client_secret' : "6a928e79291c04042271fcf06edc20ffa15f2a2bd18ef332a7d5b2a2fcc3368149722447995cb68d0c94aca5acb82e37d1b269a36530db0d7b2a9cdfd51cbfa5"
-        };
+	    if ($scope.organization === "org1") {
+            data += '&client_id='+app_org1_id;
+            data += '&client_secret='+app_org1_secret;
+	    } else if ($scope.organization === "org2") {
+            data += '&client_id='+app_org2_id;
+            data += '&client_secret='+app_org2_secret;
+        }
 
-        var jdata = JSON.stringify(formData);
-        jdata = 'grant_type=password&username=chanchan@idm.server&password=ccadmin&client_id=2&client_secret=d3d380ec8133e3b71440a0c2aec7797befef0e147b82848dffe207b7ea38a7a6d77c8854246a9853846328bb1db27dafa71ed05708f3e531095e917cd192a840';
         var url = "https://idm.server";
         var oauth_token = "oauth2/token";
         var user_roles = "user";
 
 
-        $http({method:'POST',url:url+"/"+oauth_token,data:jdata, headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+        $http({method:'POST',url:url+"/"+oauth_token,data:data, headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
         .success(function(data,status,headers,config){
             $http({method:'GET',url:url+"/"+user_roles+"?access_token="+data.access_token})
             .success(function(data,status,headers,config){
-                console.log(data);
-	    });
+               $scope.user_data =  data;
+               console.log(data);
+	        });
         }).
         error(function(data,status,headers,config){
-
           console.log(data)
-
         });
     };
 
