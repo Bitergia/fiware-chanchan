1b174c07c5f452328757ea8323d1abd18ef7aaeb
Alvaro del Castillo <acs@bitergia.com>
2014-12-15 17:04:53 +0100

    Final subscription config.

--- vagrant/scripts/cygnus/04-notify-contex-cygnus.sh
+++ vagrant/scripts/cygnus/04-notify-contex-cygnus.sh
@@ -2,6 +2,59 @@
 
 # For all the organizations, subscribe to all changes to entities with names manual:*
 # To change to params: type:org_name
+(curl localhost:10026/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "organization_a",
+            "isPattern": "true",
+            "id": "manual:*"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "http://localhost:5001/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "pressure"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
+
+(curl localhost:10026/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "organization_b",
+            "isPattern": "true",
+            "id": "manual:*"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "http://localhost:5002/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "pressure"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
+
+# Old organizations name
 
 (curl localhost:10026/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
 {
