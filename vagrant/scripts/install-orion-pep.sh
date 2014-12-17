5eb2d3a95257c236adb3019f43cf2c93f7560638
David Muriel <dmuriel@bitergia.com>
2014-12-17 18:38:56 +0100

    Added orion-pep init script.

--- vagrant/scripts/install-orion-pep.sh
+++ vagrant/scripts/install-orion-pep.sh
@@ -5,7 +5,7 @@ export ORION_PEP_SCRIPTS="${SCRIPTS_PATH}/orion-pep"
 su - ${ORION_PEP_USER} <<EOF
 # this is needed to be able to use the variables with the ${ORION_PEP_USER} user
 source ${SCRIPTS_PATH}/variables.sh
-for f in \$( ls ${ORION_PEP_SCRIPTS}/* ) ; do
+for f in \$( ls ${ORION_PEP_SCRIPTS}/*.sh ) ; do
     bash \${f}
 done
 EOF
--- vagrant/scripts/orion-pep/03-start-orion-pep.sh
+++ vagrant/scripts/orion-pep/03-start-orion-pep.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-function stop_orion_pep () {
-    orion_pep_pid=$( ps ax | grep pepProxy | grep bin | awk '{print $1}' )
-    echo ${orion_pep_pid}
-    if [ ! -z ${orion_pep_pid} ]; then
-	echo "Stopping Orion PEP"
-	kill ${orion_pep_pid}
-    fi
-}
+sudo cp ${SCRIPTS_PATH}/orion-pep/orion-pep.default /etc/default/orion-pep
+sudo sed -i /etc/default/orion-pep \
+     -e "s|^ORION_PEP_HOME=.*$|ORION_PEP_HOME=${HOME}/${ORION_PEP_HOME}|" \
+     -e "s/^ORION_PEP_USER=.*$/ORION_PEP_USER='${ORION_PEP_USER}'/" \
+     -e "s/^ORION_PEP_GROUP=.*$/ORION_PEP_GROUP='${ORION_PEP_USER}'/"
+sudo cp ${SCRIPTS_PATH}/orion-pep/orion-pep.init /etc/init.d/orion-pep
+sudo chmod +x /etc/init.d/orion-pep
+sudo update-rc.d orion-pep defaults 90 90
 
-
-function start_orion_pep () {
-    echo "Starting Orion PEP "
-    cd ${HOME}/${ORION_PEP_HOME}/
-    NODE_TLS_REJECT_UNAUTHORIZED=0  nohup bin/pepProxy 2>&1 > ${HOME}/log/orion-pep-proxy.log &
-    cd ${HOME}
-}
-
-stop_orion_pep
-start_orion_pep
+sudo service orion-pep restart
