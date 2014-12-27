21e312ba1d901e1b5fea0fdebd004918d6975582
David Muriel <dmuriel@bitergia.com>
2014-12-27 11:37:45 +0100

    Split IDM data provision.
    We need to have the OAuth2 Credentials available for Chanchan
    configuration, but also delay roles and permissions until Keypass is
    running.

--- vagrant/scripts/bootstrap.sh
+++ vagrant/scripts/bootstrap.sh
@@ -66,14 +66,17 @@ bash packages.sh
 # install IDM
 bash install-idm.sh
 
+# provision IDM app
+bash idm-provision-app.sh
+
 # install Chanchan
 bash install-chanchan.sh
 
 # install Keypass
 bash install-keypass.sh
 
-# provision IDM
-bash idm-provision.sh
+# provision IDM roles
+bash idm-provision-roles.sh
 
 if [ "${DIST_TYPE}" != "debian" ]; then
     exit 1
--- vagrant/scripts/idm-provision-roles.sh
+++ vagrant/scripts/idm-provision-roles.sh
@@ -6,35 +6,11 @@ cd ${IDM_PROVISION_SCRIPTS}
 PATH=${PATH}:${IDM_PROVISION_SCRIPTS}
 
 ORG_A="Organization A"
-ORG_A_DESC="Demo Organization A"
 ORG_B="Organization B"
-ORG_B_DESC="Demo Organization B"
 APP_NAME="Chanchan"
-APP_DESC="Demo application"
-APP_URL="http://${CC_HOSTNAME}"
-APP_CALLBACK="http://${CC_HOSTNAME}/login"
 
 ROLE_M="Member"
 ROLE_P="Publisher"
-
-# Admin user
-create-idm-user.sh "${CC_USER_NAME}" "${CC_EMAIL}" "${CC_PASS}"
-
-# Create organization A
-create-idm-org.sh "${CC_EMAIL}" "${ORG_A}" "${ORG_A_DESC}"
-
-# Add application to organization with custom role
-create-idm-app.sh "${ORG_A}" "${APP_NAME}" "${APP_DESC}" "${APP_URL}" "${APP_CALLBACK}"
-
-# Create organization B
-create-idm-org.sh "${CC_EMAIL}" "${ORG_B}" "${ORG_B_DESC}"
-
-# Add application to organization with custom role
-create-idm-app.sh "${ORG_B}" "${APP_NAME}" "${APP_DESC}" "${APP_URL}" "${APP_CALLBACK}"
-
-# Dump oauth credentials
-dump-oauth-credentials.sh "${ORG_A}" "${APP_NAME}"
-dump-oauth-credentials.sh "${ORG_B}" "${APP_NAME}"
 
 create-role.sh "${ORG_A}" "${APP_NAME}" "${ROLE_M}"
 create-role.sh "${ORG_A}" "${APP_NAME}" "${ROLE_P}"
