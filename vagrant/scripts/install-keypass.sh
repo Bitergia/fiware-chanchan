ccdbf1207720f5eca4922676d9fe6c5e3527a6d2
David Muriel <dmuriel@bitergia.com>
2015-05-12 18:39:38 +0200

    Define DIST_TYPE before using it.

--- vagrant/scripts/install-keypass.sh
+++ vagrant/scripts/install-keypass.sh
@@ -5,8 +5,8 @@ export KEYPASS_SCRIPTS="${SCRIPTS_PATH}/keypass"
 su - ${KEYPASS_USER} <<EOF
 # this is needed to be able to use the variables with the ${KEYPASS_USER} user
 export SCRIPTS_PATH=${SCRIPTS_PATH}
-source ${SCRIPTS_PATH}/variables.sh
 export DIST_TYPE=${DIST_TYPE}
+source ${SCRIPTS_PATH}/variables.sh
 for f in \$( ls ${KEYPASS_SCRIPTS}/*.sh ) ; do
     bash \${f}
 done
--- vagrant/scripts/install-orion.sh
+++ vagrant/scripts/install-orion.sh
@@ -5,8 +5,8 @@ export ORION_SCRIPTS="${SCRIPTS_PATH}/orion"
 su - ${ORION_USER} <<EOF
 # this is needed to be able to use the variables with the ${ORION_USER} user
 export SCRIPTS_PATH=${SCRIPTS_PATH}
-source ${SCRIPTS_PATH}/variables.sh
 export DIST_TYPE=${DIST_TYPE}
+source ${SCRIPTS_PATH}/variables.sh
 for f in \$( ls ${ORION_SCRIPTS}/*.sh ) ; do
     bash \${f}
 done
