2d406a453d6c17b03c4db5277b1e70b98f5b1b35
David Muriel <dmuriel@bitergia.com>
2015-05-13 13:58:43 +0200

    Install local rpms using yum to avoid rpmdb errors.

--- vagrant/scripts/packages/00-essential.sh
+++ vagrant/scripts/packages/00-essential.sh
@@ -10,7 +10,7 @@ case "${DIST_TYPE}" in
 	# we'll be installing later some packages from EPEL, so add the repository now
 	cd /tmp
 	curl -OL http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-	rpm -Uvh epel-release-6-8.noarch.rpm
+	yum localinstall -y epel-release-6-8.noarch.rpm
 	;;
     *)
 	exit 1
