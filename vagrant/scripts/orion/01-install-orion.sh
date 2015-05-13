c8f706a01a46275795837bab40d3e8c3858c0733
David Muriel <dmuriel@bitergia.com>
2015-05-13 13:53:27 +0200

    Fix orion source when compiling on 32-bit arch.

--- vagrant/scripts/orion/01-install-orion.sh
+++ vagrant/scripts/orion/01-install-orion.sh
@@ -13,4 +13,36 @@ if [ "${GIT_REV_ORION}" != "master" ]; then
     git checkout ${GIT_REV_ORION}
 fi
 
+if [[ "$(arch)" =~ i[3456]86 ]] ; then
+    # Fix compilation errors for 32 bit
+    # This is unsupported!!!
+    replace "%lu" "%llu" --  src/lib/mongoBackend/MongoCommonRegister.cpp
+    replace "%lu" "%llu" --  src/lib/mongoBackend/mongoSubscribeContext.cpp
+    replace "%lu" "%llu" --  src/lib/ngsiNotify/Notifier.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextRegistrationVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextElementVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextElementResponseVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/EntityIdVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextRegistrationAttributeVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/AttributeAssociationList.cpp
+    replace "%lu" "%u" --  src/lib/ngsi/Scope.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/NotifyConditionVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextRegistrationResponseVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ContextAttributeVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/MetadataVector.cpp
+    replace "%lu" "%llu" --  src/lib/ngsi/ScopeVector.cpp
+    replace "%lu" "%llu" --  src/lib/logMsg/logMsg.cpp
+    replace "%lu" "%llu" --  src/lib/convenience/ContextAttributeResponseVector.cpp
+    replace "%lu" "%llu" --  src/lib/parse/CompoundValueNode.cpp
+    replace "%lu" "%llu" --  src/lib/rest/clientSocketHttp.cpp
+    replace "%lu" "%llu" --  src/lib/parseArgs/paUsage.cpp
+    replace "%lu" "%llu" --  src/lib/parseArgs/parseTest.cpp
+    replace "%lu" "%llu" --  src/lib/parseArgs/paLimitCheck.cpp
+    replace "%lu" "%llu" --  src/lib/orionTypes/TypeEntityVector.cpp
+    replace "%ld" "%lld" --  src/app/proxyCoap/CoapController.cpp
+    replace "%ld" "%lld" --  src/lib/parseArgs/paUsage.cpp
+    replace "%ld" "%lld" --  src/lib/parseArgs/parseTest.cpp
+    replace "%ld" "%lld" --  src/lib/parseArgs/paLimitCheck.cpp
+fi
+
 INSTALL_DIR=${HOME} make install
