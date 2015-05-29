b2f4a6f50245461a5b14f4e7b73f16a34b9c87fb
Alvaro del Castillo <acs@bitergia.com>
2015-05-29 19:21:33 +0200

    First version of Cygnus image for Ubuntu created.

---
+++ docker/images/cygnus/notify-contex-cygnus.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# For all the organizations, subscribe to all changes to entities with names manual:*
+# To change to params: type:org_name
+(curl ${ORION_HOSTNAME}:${ORION_PORT}/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "org_a",
+            "isPattern": "true",
+            "id": "manual:*"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "http://${CYGNUS_HOSTNAME}:5001/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "pressure"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
+
+(curl ${ORION_HOSTNAME}:${ORION_PORT}/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "org_b",
+            "isPattern": "true",
+            "id": "manual:*"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "http://${CYGNUS_HOSTNAME}:5002/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "pressure"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
+
+# Santander sensors
+(curl ${ORION_HOSTNAME}:${ORION_PORT}/NGSI10/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' -d @- | python -mjson.tool) <<EOF
+{
+    "entities": [
+        {
+            "type": "santander:soundacc",
+            "isPattern": "true",
+            "id": "urn:smartsantander:testbed:*"
+        }
+    ],
+    "reference": "http://${CYGNUS_HOSTNAME}:5050/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "TimeInstant"
+            ]
+        }
+    ]
+}
+EOF
