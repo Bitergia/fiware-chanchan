62d06147117c6f86de8c1991e9e36249fceb4ea8
Alvaro del Castillo <acs@bitergia.com>
2015-06-01 18:31:01 +0200

    Create hosts.json file used to configure the web app later. Removed old config modifying js files (bad practise).

--- vagrant/scripts/chanchan/03-configure-chanchan.sh
+++ vagrant/scripts/chanchan/03-configure-chanchan.sh
@@ -2,18 +2,13 @@
 
 su - chanchan <<EOF
 sed -i ${CC_APP_SERVER_PATH}/config.js \
-  -e "/^[ ]*config.idm_url =/c config.idm_url = '${IDM_URL}';" \
   -e "/^[ ]*config.callback_url =/c config.callback_url = '${CC_APP_CALLBACK}';"
-
-sed -i ${CC_APP_CLIENT_PATH}/app/app.js \
-  -e "/^[ ]*var base_url =/c\    var base_url = '${CC_APP_URL}';"
-
 sed -i ${CC_APP_CLIENT_PATH}/app/auth/auth.js \
   -e "/^[ ]*var url =/c\        var url = '${IDM_URL}';" \
   -e "/^[ ]*\\\$scope.user =/c\    \\\$scope.user = '${CC_EMAIL}';" \
   -e "/^[ ]*\\\$scope.password =/c\    \\\$scope.password = '${CC_PASS}';"
-
 EOF
+
 if [ -f "${CC_OAUTH_CREDENTIALS}" ]; then
     ORGANIZATIONS=$( (echo "{" ; (cat "${CC_OAUTH_CREDENTIALS}" | paste -s -d ',') ; echo "}")  | python -m json.tool )
     su - chanchan <<EOF
@@ -22,3 +17,10 @@ ${ORGANIZATIONS}
 __EOF
 EOF
 fi
+
+HOSTS=$(echo '{"chanchan":"'${CC_HOSTNAME}'","idm":"'$IDM_HOSTNAME'"}')
+su - chanchan <<EOF
+cat << __EOF > ${CC_APP_CLIENT_PATH}/app/hosts.json
+${HOSTS}
+__EOF
+EOF
