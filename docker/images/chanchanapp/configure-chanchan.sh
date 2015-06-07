b3923c7e425fd82c0b905b29b9e8fb075b57344b
Alvaro del Castillo <acs@bitergia.com>
2015-06-07 09:06:25 +0200

    Converting images to share config between containers.

--- docker/images/chanchanapp/configure-chanchan.sh
+++ docker/images/chanchanapp/configure-chanchan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CC_APP_CLIENT_PATH="fiware-chanchan/client"
-CC_OAUTH_CREDENTIALS="/tmp/appoauth.txt"
+# CC_OAUTH_CREDENTIALS="/tmp/appoauth.txt"
+CC_OAUTH_CREDENTIALS="/config/appoauth.txt"
 DEPLOY_USER=bitergia
 
 if [ -f "${CC_OAUTH_CREDENTIALS}" ]; then
