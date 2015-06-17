53b7d3589562efd7acb2fbc2424cb860258b1e60
Alvaro del Castillo <acs@bitergia.com>
2015-06-17 09:51:10 +0200

    Added execution of IDAS scripts to REST API. Working.

--- server/routes/idas.js
+++ server/routes/idas.js
@@ -18,5 +18,41 @@ exports.update_temperature = function(req, res) {
 
     var device_id = req.params.device_id;
     var temperature = req.params.temperature;
-    res.send("OK")
+
+    var scripts_home = "/home/bitergia/scripts/idas-scripts";
+    var idas_params = " --idas-host idas --idas-port 8080 ";
+    var orion_params = " --context-broker-url http://orion:1026 ";
+    var api_params = " --api-key test ";
+    var service_params = " --service bitergiaidas --service-path / ";
+
+    var cmd_service = scripts_home+"/create_service.sh ";
+    cmd_service += orion_params + idas_params + api_params + service_params;
+    console.log(cmd_service);
+
+    var cmd_device = scripts_home+"/create_temp_device.sh ";
+    cmd_device += idas_params + service_params;
+    cmd_device += "--device c4:8e:8f:f4:38:2b:Temp_1 ";
+    cmd_device += "--entity SENSOR_TEMP_c4_8e_8f_f4_38_2b_Temperature_Sensor_1";
+    console.log(cmd_device);
+
+    var cmd_send = scripts_home+"/send_data.sh ";
+    cmd_send += idas_params;
+    cmd_send += service_params;
+    cmd_send += api_params;
+    cmd_send += "--device "+device_id+" "; // c4:8e:8f:f4:38:2b:Temp_1
+    // cmd += "--measurement \"t$(./get_temp.sh)\""
+    cmd_send += "--measurement \"t|"+ temperature + "\""
+    console.log(cmd_send);
+
+    var exec = require('child_process').exec, child;
+
+    child = exec(cmd_send,
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+        res.send(cmd_send)
+    });
 };
