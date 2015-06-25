720661e49202aef4226966aa72fccbbd66c38c3e
Alberto Martín <alberto.martin@bitergia.com>
2015-06-25 20:21:11 +0200

    Added connection tests and minor bugfixes

--- docker/images/idm/config-domain.sh
+++ docker/images/idm/config-domain.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Sleep 5 seconds to let the Authzforce interface set-up
-
-sleep 5
+AUTHZFORCE_HOSTNAME=authzforce
+AUTHZFORCE_PORT=8080
+TIMEOUT=10
+try=0
+ok=0
 
 # Request to Authzforce to retrieve Domain
 
-DOMAIN="$(curl -s --request GET http://authzforce:8080/authzforce/domains | awk '/href/{print $NF}' | cut -d '"' -f2)" 
-
-# Checks if the Domain exists. If not, creates one
-
-if [ -z "$DOMAIN" ]; then 
-    echo "Domain is not created yet!"
-    curl -s --request POST --header "Content-Type: application/xml;charset=UTF-8" --data '<?xml version="1.0" encoding="UTF-8"?><taz:properties xmlns:taz="http://thalesgroup.com/authz/model/3.0/resource"><name>MyDomain</name><description>This is my domain.</description></taz:properties>' --header "Accept: application/xml" http://authzforce:8080/authzforce/domains
-    DOMAIN="$(curl -s --request GET http://authzforce:8080/authzforce/domains | awk '/href/{print $NF}' | cut -d '"' -f2)"
-    echo $DOMAIN
-else
-    echo "Domain value is not empty: "
-    echo $DOMAIN
+try=0
+ok=0
+echo "Testing Authzforce url"
+while [ $try -lt $TIMEOUT -a $ok -eq 0 ] ; do
+	echo "Checking if Authzforce is ready at ${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT}/authzforce/domains (try $try)..."
+	if curl ${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT}/authzforce/domains -s | grep -q href ; then
+	    ok=1
+            DOMAIN="$(curl -s --request GET http://${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT}/authzforce/domains | awk '/href/{print $NF}' | cut -d '"' -f2)"
+	else
+	    sleep 1
+	    try=$(( $try + 1 ))
+	fi
+done
+if [ ! $ok ] ; then
+        echo "Failed to connect to Authzforce at ${AUHTZFORCE_HOSTNAME}:${AUTHFORCE_PORT}"
+        exit 1
 fi
 
-# TODO: retrieve magic_key automatically
+# Parse the value into the IdM settings
 
 sed -e "s@^ACCESS_CONTROL_URL = None@ACCESS_CONTROL_URL = 'http://authzforce:8080/authzforce/domains/${DOMAIN}/pap/policySet'@" -i /opt/fi-ware-idm/horizon/openstack_dashboard/local/local_settings.py
 sed -e "s@^ACCESS_CONTROL_MAGIC_KEY = None@ACCESS_CONTROL_MAGIC_KEY = 'daf26216c5434a0a80f392ed9165b3b4'@" -i /opt/fi-ware-idm/horizon/openstack_dashboard/local/local_settings.py
--- docker/images/wilma-pep/config-domain.sh
+++ docker/images/wilma-pep/config-domain.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 
-# Sleep 10 seconds to let the Authzforce interface set-up/
+AUTHZFORCE_HOSTNAME=authzforce
+AUTHZFORCE_PORT=8080
+TIMEOUT=10
+try=0
+ok=0
 
-sleep 10
+echo "Testing Authzforce port"
+while [ $try -lt $TIMEOUT -a $ok -eq 0 ] ; do
+        echo "Checking connection with Authzforce at ${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT} (try $try)..."
+        if nc -z -w $TIMEOUT $AUTHZFORCE_HOSTNAME $AUTHZFORCE_PORT ; then
+            # Authzforce is up
+            ok=1
+        else
+            # keep waiting
+            sleep 1
+            try=$(( $try + 1 ))
+        fi
+done
+if [ ! $ok ] ; then
+        echo "Failed to connect to Authzforce at ${AUHTZFORCE_HOSTNAME}:${AUTHFORCE_PORT}"
+        exit 1
+fi
+
+# Request to Authzforce to retrieve Domain
+
+try=0
+ok=0
+echo "Testing Authzforce url"
+while [ $try -lt $TIMEOUT -a $ok -eq 0 ] ; do
+        echo "Checking if Authzforce is ready at ${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT}/authzforce/domains (try $try)..."
+        if curl ${AUTHZFORCE_HOSTNAME}:${AUTHZFORCE_PORT}/authzforce/domains -s | grep -q href ; then
+            ok=1
+            echo "Authzforce is ready!"
+        else
+            sleep 1
+            try=$(( $try + 1 ))
+        fi
+done
+if [ ! $ok ] ; then
+        echo "Failed to connect to Authzforce at ${AUHTZFORCE_HOSTNAME}:${AUTHFORCE_PORT}"
+        exit 1
+fi
 
 # Request to Authzforce to retrieve Domain
 
@@ -12,9 +51,9 @@ DOMAIN="$(curl -s --request GET http://authzforce:8080/authzforce/domains | awk 
 
 if [ -z "$DOMAIN" ]; then 
     echo "Domain is not created yet!"
-    curl -s --request POST --header "Content-Type: application/xml;charset=UTF-8" --data '<?xml version="1.0" encoding="UTF-8"?><taz:properties xmlns:taz="http://thalesgroup.com/authz/model/3.0/resource"><name>MyDomain</name><description>This is my domain.</description></taz:properties>' --header "Accept: application/xml" http://authzforce:8080/authzforce/domains
+    curl -s --request POST --header "Content-Type: application/xml;charset=UTF-8" --data '<?xml version="1.0" encoding="UTF-8"?><taz:properties xmlns:taz="http://thalesgroup.com/authz/model/3.0/resource"><name>MyDomain</name><description>This is my domain.</description></taz:properties>' --header "Accept: application/xml" http://authzforce:8080/authzforce/domains --output /dev/null
     DOMAIN="$(curl -s --request GET http://authzforce:8080/authzforce/domains | awk '/href/{print $NF}' | cut -d '"' -f2)"
-    echo $DOMAIN
+    echo "Domain has been created: $DOMAIN"
 else
     echo "Domain value is not empty: "
     echo $DOMAIN
@@ -24,14 +63,40 @@ fi
 
 sed -e "s@^    path:@    path:'/authzforce/domains/$DOMAIN/pdp'@" -i /opt/fi-ware-pep-proxy/config.js
 
-# TEMP Perform the actions needed in IdM for the PEP authentication
-# TODO Implement this actions while deploying IdM
+# Check that IdM is up and responsive 
 
-sleep 10
+IDM_HOSTNAME=idm
+IDM_PORT=5000
+try=0
+ok=0
 
-FRESHTOKEN="$(curl -s -i   -H "Content-Type: application/json"   -d '{ "auth": {"identity": {"methods": ["password"], "password": { "user": { "name": "pepproxy@test.com", "domain": { "id": "default" }, "password": "test"} } } } }' http://idm:5000/v3/auth/tokens | grep ^X-Subject-Token: | awk '{print $2}')"
-MEMBERID="$(curl -s -H "X-Auth-Token:$FRESHTOKEN" -H "Content-type: application/json" http://idm:5000/v3/roles | python -m json.tool | grep -iw id | awk -F'"' '{print $4}' | head -n 1)"
-REQUEST="$(curl -s -X PUT -H "X-Auth-Token:$FRESHTOKEN" -H "Content-type: application/json" http://idm:5000/v3/domains/default/users/pepproxy/roles/${MEMBERID})"
+echo "Testing Keystone port"
+while [ $try -lt $TIMEOUT -a $ok -eq 0 ] ; do
+        echo "Checking connection with Keystone at ${IDM_HOSTNAME}:${IDM_PORT} (try $try)..."
+        if nc -z -w $TIMEOUT $IDM_HOSTNAME $IDM_PORT ; then
+            # Keystone is up
+            ok=1
+            echo "Keystone is ready!"
+        else
+            # keep waiting
+            sleep 1
+            try=$(( $try  1 ))
+        fi
+done
+if [ ! $ok ] ; then
+        echo "Failed to connect to Keystone at ${IDM_HOSTNAME}:${IDM_PORT}"
+        exit 1
+fi
+
+# Configure Domain permissions to user 'pepproxy' at IdM
+
+FRESHTOKEN="$(curl -s -i   -H "Content-Type: application/json"   -d '{ "auth": {"identity": {"methods": ["password"], "password": { "user": { "name": "idm", "domain": { "id": "default" }, "password": "idm"} } } } }' http://${IDM_HOSTNAME}:${IDM_PORT}/v3/auth/tokens | grep ^X-Subject-Token: | awk '{print $2}')"
+MEMBERID="$(curl -s -H "X-Auth-Token:$FRESHTOKEN" -H "Content-type: application/json" http://${IDM_HOSTNAME}:${IDM_PORT}/v3/roles | python -m json.tool | grep -iw id | awk -F'"' '{print $4}' | head -n 1)"
+REQUEST="$(curl -s -X PUT -H "X-Auth-Token:$FRESHTOKEN" -H "Content-type: application/json" http://${IDM_HOSTNAME}:${IDM_PORT}/v3/domains/default/users/pepproxy/roles/${MEMBERID})"
+
+echo "User pepproxy has been granted with:"
+echo "Role: ${MEMBERID}"
+echo "Token:  $FRESHTOKEN"
 
 # Start container back
 
